0de7e1fefbc4a75d9bef9fd2a81f8d154de748d3
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-04 21:53:52 -0400

    Initialize files

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO books (id, book_name)
+VALUES 
+    (1, "Pride and Prejudice"), 
+    (2, "World War Z"), 
+    (3, "The Great Gatsby"), 
+    (4, "Catcher in the Rye"), 
+    (5, "Jurrasic Park");
+
+INSERT INTO categories (id, category_name)
+VALUES 
+    (1, "Fiction"),
+    (2, "Non-Fiction");
+
+INSERT INTO prices (id, price) VALUES (1, 10.55);
